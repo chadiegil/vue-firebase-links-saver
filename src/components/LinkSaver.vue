c52dc3fd58bb6d264c3d32745119b1354aaa8fa8
Chadie Gil Augis <chadie.mail@gmail.com>
2023-07-09 16:19:53 +0800

    update link with copy functionality

--- src/components/LinkSaver.vue
+++ src/components/LinkSaver.vue
@@ -50,18 +50,13 @@
     <div class="container_card">
       <div v-for="link in linksList" :key="link.id" class="info">
         <div class="info__icon">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            viewBox="0 0 24 24"
-            height="24"
-            fill="none"
+          <span
+            class="material-symbols-outlined"
+            style="color: white"
+            @click="copytext(link.link)"
           >
-            <path
-              fill="#393a37"
-              d="m12 1.5c-5.79844 0-10.5 4.70156-10.5 10.5 0 5.7984 4.70156 10.5 10.5 10.5 5.7984 0 10.5-4.7016 10.5-10.5 0-5.79844-4.7016-10.5-10.5-10.5zm.75 15.5625c0 .1031-.0844.1875-.1875.1875h-1.125c-.1031 0-.1875-.0844-.1875-.1875v-6.375c0-.1031.0844-.1875.1875-.1875h1.125c.1031 0 .1875.0844.1875.1875zm-.75-8.0625c-.2944-.00601-.5747-.12718-.7808-.3375-.206-.21032-.3215-.49305-.3215-.7875s.1155-.57718.3215-.7875c.2061-.21032.4864-.33149.7808-.3375.2944.00601.5747.12718.7808.3375.206.21032.3215.49305.3215.7875s-.1155.57718-.3215.7875c-.2061.21032-.4864.33149-.7808.3375z"
-            ></path>
-          </svg>
+            file_copy
+          </span>
         </div>
         <div class="info__title">{{ link.link }}</div>
         <div class="info__close" @click="deletePost(link.id)">
@@ -152,6 +147,19 @@ const deletePost = async (id) => {
   fetchData();
 };
 
+const copytext = (text) => {
+  navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      console.log("Text copied:", text);
+      // You can show a success message or perform additional actions here
+    })
+    .catch((error) => {
+      console.error("Error copying text:", error);
+      // You can show an error message or handle the error here
+    });
+};
+
 // getting the links from the firebase firestore
 onMounted(() => {
   fetchData();
@@ -159,6 +167,10 @@ onMounted(() => {
 </script>
 
 <style scoped>
+.material-symbols-outlined {
+  font-variation-settings: "FILL" 0, "wght" 400, "GRAD" 0, "opsz" 48;
+}
+
 .container {
   width: 100%;
   height: 100px;
@@ -288,6 +300,7 @@ onMounted(() => {
 .info__icon {
   width: 20px;
   height: 20px;
+  display: flex;
   transform: translateY(-2px);
   margin-right: 8px;
 }
