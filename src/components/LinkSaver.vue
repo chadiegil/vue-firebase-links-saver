370f47e6db5007b6690faccda8c7dda9e207a6b2
Chadie Gil Augis <chadie.mail@gmail.com>
2023-07-09 16:09:25 +0800

    update links width

--- src/components/LinkSaver.vue
+++ src/components/LinkSaver.vue
@@ -47,35 +47,36 @@
         </form>
       </div>
     </div>
-
-    <div v-for="link in linksList" :key="link.id" class="info">
-      <div class="info__icon">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          viewBox="0 0 24 24"
-          height="24"
-          fill="none"
-        >
-          <path
-            fill="#393a37"
-            d="m12 1.5c-5.79844 0-10.5 4.70156-10.5 10.5 0 5.7984 4.70156 10.5 10.5 10.5 5.7984 0 10.5-4.7016 10.5-10.5 0-5.79844-4.7016-10.5-10.5-10.5zm.75 15.5625c0 .1031-.0844.1875-.1875.1875h-1.125c-.1031 0-.1875-.0844-.1875-.1875v-6.375c0-.1031.0844-.1875.1875-.1875h1.125c.1031 0 .1875.0844.1875.1875zm-.75-8.0625c-.2944-.00601-.5747-.12718-.7808-.3375-.206-.21032-.3215-.49305-.3215-.7875s.1155-.57718.3215-.7875c.2061-.21032.4864-.33149.7808-.3375.2944.00601.5747.12718.7808.3375.206.21032.3215.49305.3215.7875s-.1155.57718-.3215.7875c-.2061.21032-.4864.33149-.7808.3375z"
-          ></path>
-        </svg>
-      </div>
-      <div class="info__title">{{ link.link }}</div>
-      <div class="info__close" @click="deletePost(link.id)">
-        <svg
-          height="20"
-          viewBox="0 0 20 20"
-          width="20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="m15.8333 5.34166-1.175-1.175-4.6583 4.65834-4.65833-4.65834-1.175 1.175 4.65833 4.65834-4.65833 4.6583 1.175 1.175 4.65833-4.6583 4.6583 4.6583 1.175-1.175-4.6583-4.6583z"
-            fill="#393a37"
-          ></path>
-        </svg>
+    <div class="container_card">
+      <div v-for="link in linksList" :key="link.id" class="info">
+        <div class="info__icon">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            viewBox="0 0 24 24"
+            height="24"
+            fill="none"
+          >
+            <path
+              fill="#393a37"
+              d="m12 1.5c-5.79844 0-10.5 4.70156-10.5 10.5 0 5.7984 4.70156 10.5 10.5 10.5 5.7984 0 10.5-4.7016 10.5-10.5 0-5.79844-4.7016-10.5-10.5-10.5zm.75 15.5625c0 .1031-.0844.1875-.1875.1875h-1.125c-.1031 0-.1875-.0844-.1875-.1875v-6.375c0-.1031.0844-.1875.1875-.1875h1.125c.1031 0 .1875.0844.1875.1875zm-.75-8.0625c-.2944-.00601-.5747-.12718-.7808-.3375-.206-.21032-.3215-.49305-.3215-.7875s.1155-.57718.3215-.7875c.2061-.21032.4864-.33149.7808-.3375.2944.00601.5747.12718.7808.3375.206.21032.3215.49305.3215.7875s-.1155.57718-.3215.7875c-.2061.21032-.4864.33149-.7808.3375z"
+            ></path>
+          </svg>
+        </div>
+        <div class="info__title">{{ link.link }}</div>
+        <div class="info__close" @click="deletePost(link.id)">
+          <svg
+            height="20"
+            viewBox="0 0 20 20"
+            width="20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="m15.8333 5.34166-1.175-1.175-4.6583 4.65834-4.65833-4.65834-1.175 1.175 4.65833 4.65834-4.65833 4.6583 1.175 1.175 4.65833-4.6583 4.6583 4.6583 1.175-1.175-4.6583-4.6583z"
+              fill="#393a37"
+            ></path>
+          </svg>
+        </div>
       </div>
     </div>
   </div>
@@ -264,20 +265,21 @@ onMounted(() => {
 }
 
 /* Card */
+.container_card {
+  width: 100%;
+}
 .info {
   font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
     Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  width: 320px;
+  width: 89%;
   padding: 12px;
   display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: start;
   background: #509af8;
   border-radius: 8px;
   box-shadow: 0px 0px 5px -3px #111;
   margin: 10px;
 }
+
 .info:hover {
   background: #2d82f1;
   cursor: pointer;
